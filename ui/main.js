11ebe30f2ba10f6d34fdcf274bda53e0417de8d6
rajeshswarna123 <rajeshswarna123@gmail.com>
2017-02-08 12:13:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_butn");
+submit.onclick=function(){
+    
+    
+};
