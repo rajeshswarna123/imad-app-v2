79968801d4009dbf65c1fca4d35f81cf71dabaa5
rajeshswarna123 <rajeshswarna123@gmail.com>
2017-02-08 12:21:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ var nameInput=document.getElementById("name");
 var name=nameInput.value;
 var submit=document.getElementById("submit_butn");
 submit.onclick=function(){
-    
-    
+    var names=['name1','name2','name3'];
+    var list="";
+    for(var i=0;i<names.length;i++){
+    list+= '<li>' +name[i]+ '</li>';
+}
+var ul = document.getElementById('namrlist');
+ui.innerHTML=list;
 };
