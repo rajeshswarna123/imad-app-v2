e0f344cdcd0f6fa175dc004e3079c913bfedfd55
rajeshswarna123 <rajeshswarna123@gmail.com>
2017-03-07 19:13:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,8 +23,15 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input/', function (req, res){
+var hashedString=hash(req.params.input,'this-is-rajesh');
+res.send(hashedString);
+    
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
